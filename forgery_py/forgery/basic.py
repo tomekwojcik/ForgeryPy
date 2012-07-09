703f2fcb713a1314561faa5fab866f36b37533ef
Tomek Wójcik <labs@tomekwojcik.pl>
2012-07-09 22:16:58 +0200

    * Refactored basic forgeries into a mixin.

--- forgery_py/forgery/basic.py
+++ forgery_py/forgery/basic.py
@@ -5,43 +5,42 @@ import string
 
 HEX_DIGITS = string.hexdigits[:-6].upper()
 
-__all__ = ['hex_color', 'hex_color_short', 'text']
+__all__ = ['BasicForgeryMixin']
 
 
-def hex_color():
-    return ''.join(random.sample(HEX_DIGITS, 6))
+class BasicForgeryMixin(object):
+    def hex_color(self):
+        return ''.join(random.sample(HEX_DIGITS, 6))
 
+    def hex_color_short(self):
+        return ''.join(random.sample(HEX_DIGITS, 3))
 
-def hex_color_short():
-    return ''.join(random.sample(HEX_DIGITS, 3))
+    def text(self, length=None, at_least=10, at_most=15, lowercase=True,
+             uppercase=True, digits=True, spaces=True, punctuation=False):
+        base_string = ''
+        if lowercase:
+            base_string += string.ascii_lowercase
 
+        if uppercase:
+            base_string += string.ascii_uppercase
 
-def text(length=None, at_least=10, at_most=15, lowercase=True, uppercase=True, digits=True,
-         spaces=True, punctuation=False):
-    base_string = ''
-    if lowercase:
-        base_string += string.ascii_lowercase
+        if digits:
+            base_string += string.digits
 
-    if uppercase:
-        base_string += string.ascii_uppercase
+        if spaces:
+            base_string += ' '
 
-    if digits:
-        base_string += string.digits
+        if punctuation:
+            base_string += string.punctuation
 
-    if spaces:
-        base_string += ' '
+        if len(base_string) == 0:
+            return ''
 
-    if punctuation:
-        base_string += string.punctuation
+        if not length:
+            length = random.randint(at_least, at_most)
 
-    if len(base_string) == 0:
-        return ''
+        result = ''
+        for i in xrange(0, length):
+            result += random.choice(base_string)
 
-    if not length:
-        length = random.randint(at_least, at_most)
-
-    result = ''
-    for i in xrange(0, length):
-        result += random.choice(base_string)
-
-    return result
+        return result
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,44 +3,48 @@
 import re
 from unittest import TestCase
 
-from forgery_py.forgery import basic
+from forgery_py.forgery.basic import BasicForgeryMixin
 
 
 class BasicForgeryTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.basic = BasicForgeryMixin()
+        
     def test_hex_color(self):
-        color = basic.hex_color()
+        color = self.basic.hex_color()
 
         assert re.match(r'[0-9A-F]{6}$', color) is not None
 
     def test_hex_color_short(self):
-        color = basic.hex_color_short()
+        color = self.basic.hex_color_short()
 
         assert re.match(r'[0-9A-F]{3}$', color) is not None
 
     def test_text(self):
-        text1 = basic.text(length=10)
+        text1 = self.basic.text(length=10)
         assert len(text1) == 10
 
-        text2 = basic.text(at_least=10, at_most=15)
+        text2 = self.basic.text(at_least=10, at_most=15)
         assert len(text2) >= 10 and len(text2) <= 15
 
-        text3 = basic.text(length=26, lowercase=False, uppercase=True,
+        text3 = self.basic.text(length=26, lowercase=False, uppercase=True,
                            digits=False, spaces=False, punctuation=False)
         assert re.match(r'[A-Z]{26}$', text3) is not None
 
-        text4 = basic.text(length=26, lowercase=True, uppercase=False,
+        text4 = self.basic.text(length=26, lowercase=True, uppercase=False,
                            digits=False, spaces=False, punctuation=False)
         assert re.match(r'[a-z]{26}$', text4) is not None
 
-        text5 = basic.text(length=10, lowercase=False, uppercase=False,
+        text5 = self.basic.text(length=10, lowercase=False, uppercase=False,
                            digits=True, spaces=False, punctuation=False)
         assert re.match(r'[0-9]{10}$', text5) is not None
 
-        text6 = basic.text(length=1, lowercase=False, uppercase=False,
+        text6 = self.basic.text(length=1, lowercase=False, uppercase=False,
                            digits=False, spaces=True, punctuation=False)
         assert text6 == ' '
 
-        text7 = basic.text(length=32, lowercase=False, uppercase=False,
+        text7 = self.basic.text(length=32, lowercase=False, uppercase=False,
                            digits=False, spaces=False, punctuation=True)
         assert re.match(r"""[!"#$%&\\'()*+,-\.\/:;<=>?@\[\]^_`{|}~]{32}$""",
                         text7) is not None
