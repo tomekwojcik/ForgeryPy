01a795d54b971252a89b661b313290b6a41770ca
Tomek Wójcik <labs@tomekwojcik.pl>
2012-07-10 22:58:40 +0200

    * Package-level imports.

--- forgery_py/__init__.py
+++ forgery_py/__init__.py
@@ -2,3 +2,9 @@
 
 from .forgery import address
 from .forgery import basic
+from .forgery import currency
+from .forgery import date
+from .forgery import internet
+from .forgery import lorem_ipsum
+from .forgery import name
+from .forgery import personal
